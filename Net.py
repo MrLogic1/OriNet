8473fc933ada0424f9d35f61adab80ed7f846944
beautifulcat <781878580@qq.com>
2023-03-08 16:22:27 +0800

    框架大体搭建完成

--- Net.py
+++ Net.py
@@ -1,9 +1,75 @@
 import torch
 from torch import nn
 from torchvision import models
-class QueryEncoder():
-    print("queryencoder")
+class QueryEncoder(nn.Module):
+    def __init__(self, out_dim=128):
+        super().__init__()
+        self.dim = out_dim
+        self.resnet = models.resnet50(weights=None)
 
+        fc_feature = self.resnet.fc.in_features
+
+        self.resnet.fc = nn.Sequential(
+            nn.BatchNorm1d(fc_feature * 1),
+            nn.Linear(fc_feature * 1, self.dim)
+        )
+    def forward(self, input):
+        embeddings = self.resnet(input)
+        embeddings = torch.nn.functional.normalize(embeddings, p = 2, dim = 1)
+        return embeddings
+class ImgClsNet(nn.Module):
+    def __init__(self):
+        out_dim = 128
+        dim1 = 64
+        dim2 = 16
+        p = 0.2
+        super().__init__()
+        self.fc1 = nn.Linear(out_dim, dim1)
+        self.relu = nn.ReLU()
+        self.drop_out = nn.Dropout(p = p)
+        self.fc2 = nn.Linear(dim1, dim2)
+
+    def forward(self, input):
+        #要不要激活呢？
+        out1 = self.relu(input)
+        out1 = self.drop_out(out1)
+        out1 = self.fc1(out1)
+
+        out2 = self.relu(out1)
+        out2 = self.drop_out(out2)
+        out2 = self.fc2(out2)
+
+        return out1, out2
+
+class ImgOriNet(nn.Module):
+    def __init__(self):
+        out_dim = 128
+        dim = 16
+        p = 0.2
+        super().__init__()
+        self.sequential = nn.Sequential(
+            nn.ReLU(),
+            nn.Dropout(p = p),
+            nn.Linear(out_dim, dim),
+        )
+    def forward(self, input):
+        out = self.sequential(input)
+        return out
+
+class ImgNet(nn.Module):
+    def __init__(self):
+        out_dim = 128
+
+        super().__init__()
+        self.query_encoder = QueryEncoder(out_dim)
+        self.clf = ImgClsNet()
+        self.ori = ImgOriNet()
+    def forward(self, input):
+        out = self.query_encoder(input)
+        o1 = self.ori(out)
+        o2, o3 = self.clf(out)
+
+        return o1,o2,o3
 class RenderEncoder(nn.Module):
     def __init__(self, out_dim=128):
         super().__init__()
@@ -20,14 +86,83 @@ class RenderEncoder(nn.Module):
         embeddings = self.resnet(input)
         embeddings = torch.nn.functional.normalize(embeddings, p = 2, dim = 1)
         return embeddings
+class ModelClfNet(nn.Module):
+    def __init__(self):
+        out_dim = 128
+        dim1 = 64
+        dim2 = 16
+        p = 0.2
+        super().__init__()
+        self.fc1 = nn.Linear(out_dim, dim1)
+        self.relu = nn.ReLU()
+        self.drop_out = nn.Dropout(p = p)
+        self.fc2 = nn.Linear(dim1, dim2)
 
+    def forward(self, input):
+        #要不要激活呢？
+        out1 = self.relu(input)
+        out1 = self.drop_out(out1)
+        out1 = self.fc1(out1)
+
+        out2 = self.relu(out1)
+        out2 = self.drop_out(out2)
+        out2 = self.fc2(out2)
+
+        return out1, out2
+class ModelOriNet(nn.Module):
+    def __init__(self):
+        out_dim = 128
+        dim = 16
+        p = 0.2
+        super().__init__()
+        self.sequential = nn.Sequential(
+            nn.ReLU(),
+            nn.Dropout(p = p),
+            nn.Linear(out_dim, dim),
+        )
+    def forward(self, input):
+        out = self.sequential(input)
+        return out
+class ModelNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.render_encoder = RenderEncoder()
+        self.clf_net = ModelClfNet()
+        self.ori_net = ModelOriNet()
+    def forward(self, input):
+        out = self.render_encoder(input)
+        out1 = self.ori_net(out)
+        out2, out3 = self.clf_net(out)
+
+        return out1, out2, out3
 if __name__ == '__main__':
-    pre = torch.load("./test/pretrained/resnet50-11ad3fa6.pth")
-    r = RenderEncoder()
-    print(r.resnet)
-    # r.resnet.load_state_dict(pre)
-    # print(r.children())
-    # for name,parameters in r.named_parameters():
-    #     # print("a")
-    #     print(name)
 
+    net1 = ImgNet().to(device='cuda')
+    net2 = ModelNet().to(device='cuda')
+    x1 = torch.randn(100, 8, 3, 224, 224,requires_grad=True,device='cuda')
+    y1 = torch.randn(100, 8, 16,requires_grad=False,device='cuda')
+
+    x2 = torch.randn(100, 8, 3, 224, 224,requires_grad=True, device='cuda')
+    y2 = torch.randn(100, 8, 16,requires_grad=False, device='cuda')
+
+    loss = nn.MSELoss()
+
+    opt1 = torch.optim.Adam(net1.parameters())
+    opt2 = torch.optim.Adam(net2.parameters())
+
+    for i in range (100):
+        o11, o12, o13 = net1(x1[i])
+        o21, o22, o23 = net2(x2[i])
+        l1 = loss(o13, y1[i])
+        l2 = loss(o23, y2[i])
+
+        l3 = loss(o12,o22)
+        l4 = loss(o11,o21)
+
+        l = l1 + l2 + l3 + l4
+        l.backward()
+
+        opt1.step()
+        opt2.step()
+
+        print(l.item())
